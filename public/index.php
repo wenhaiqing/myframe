7066b3128f8a2b26ba2b371d8b236b99dce2dce4
wenhaiqing <243083741@qq.com>
2016-12-06 10:42:24 +0800

    新增了路由组件

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhiduoxin6
+ * Date: 2016/12/6
+ * Time: 10:38
+ */
+// Autoload 自动载入
+require '../vendor/autoload.php';
+
+// 路由配置
+require '../config/routes.php';
